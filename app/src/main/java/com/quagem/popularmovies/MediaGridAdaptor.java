b03130060c56ae57897b6edbe42a966bc21299dd
Antonio Narvaez <oinzea@hotmail.com>
2018-05-02 08:33:10 -0400

    Trying to use progressbar with Picasso.

--- app/src/main/java/com/quagem/popularmovies/MediaGridAdaptor.java
+++ app/src/main/java/com/quagem/popularmovies/MediaGridAdaptor.java
@@ -1,12 +1,18 @@
 package com.quagem.popularmovies;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.List;
 
@@ -38,21 +44,37 @@ public class MediaGridAdaptor extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
 
-        if (view == null)
+        //if (view == null)
             view = activity.getLayoutInflater().inflate(
                     R.layout.grid_item, viewGroup, false);
 
-        ImageView imageView = view.findViewById(R.id.iv_movie_poster);
+        final ImageView imageView = view.findViewById(R.id.iv_movie_poster);
+        final ProgressBar progressBar = view.findViewById(R.id.progressbar);
 
         Picasso.with(activity)
-                .load(TMDBNetworkTools.getImageUri(
-                        listData.get(i).getPosterPath(),
+                .load(TMDBNetworkTools.getImageUri(listData.get(i).getPosterPath(),
                         TMDBNetworkTools.TMDB_IMAGE_W185))
-                .placeholder(R.drawable.ic_launcher_background) // TODO: 4/27/18
-                .error(R.drawable.ic_launcher_background) // TODO: 4/27/18
-                .into(imageView);
 
-        return imageView;
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        imageView.setImageDrawable(new BitmapDrawable(
+                                activity.getResources(), bitmap));
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) {
+
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+                       progressBar.setVisibility(View.VISIBLE);
+                    }
+                });
+
+        return view;
     }
 
     @Override
